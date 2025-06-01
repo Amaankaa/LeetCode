5325202e9949cb29d92506d5758aac41c76dff4e
Amanuel Merara <amaanumararaa@gmail.com>
2025-06-01 15:05:31 +0300

    Time: 3 ms (15.28%), Space: 18.1 MB (20.86%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        res = []
+        for log in logs:
+            if log == "../" and res:
+                res.pop()
+            elif log == "./" or (log == "../" and not res):
+                continue
+            else:
+                res.append('a')
+        return len(res) if res else 0
