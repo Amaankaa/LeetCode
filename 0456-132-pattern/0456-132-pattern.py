c1f144a82fef92aa2c87afe6341865621d075363
Amanuel Merara <amaanumararaa@gmail.com>
2025-06-04 23:03:30 +0300

    Time: 35 ms (98.81%), Space: 35.5 MB (13.57%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        two = float('-inf')
+        
+        for num in reversed(nums):
+            if num < two:
+                return True
+            
+            while stack and num > stack[-1]:
+                two = stack.pop()
+            
+            stack.append(num)
+        
+        return False
